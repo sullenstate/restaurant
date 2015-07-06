8acb1902509b840684376cbfe5bfef358e0b297a
morganthomas <morgan.a.s.thomas@gmail.com>
2015-07-06 17:35:29 -0600

    Started restaurant UI.

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-// $(document).on('ready', function() {
-  
-// });
-
 var showFlag = function(label, flag){
 	return flag ? label : 'not ' + label;
 };
+
+//
+// FoodItem
+//
 
 var FoodItem = function(name, calories, vegan, glutenFree, citrusFree){
 	this.name = name;
@@ -19,28 +19,18 @@ FoodItem.prototype.toString = function(){
 	  showFlag('gluten free', this.glutenFree) + ', ' + showFlag('citrus free', this.citrusFree) + '.';
 };
 
-var Drink = function(name, description, price, ingredients){
-	this.name = name;
-	this.description = description;
-	this.price = price;
-	this.ingredients = ingredients;
-};
+// FoodItem.prototype.create = function() {
+//   $("<div></div>")
+// }
+
+//
+// Drink and Plate
+//
 
 var drinkOrPlateToString = function(){
 	return _.capitalize(this.name) + ' ($' + this.price.toFixed(2) + '). ' + this.description + ' Ingredients: '
 	+ _.capitalize(this.ingredients.map(_.property('name')).join(', ')) + '.';
 };
-
-Drink.prototype.toString = drinkOrPlateToString;
-
-var Plate = function(name, description, price, ingredients){
-	this.name = name;
-	this.description = description;
-	this.price = price;
-	this.ingredients = ingredients;
-};
-
-Plate.prototype.toString = drinkOrPlateToString;
 
 var all = function(bools){
 	return bools.reduce(function(a, b){
@@ -54,9 +44,39 @@ var makeDietChecker = function(restriction){
 	};
 };
 
+var Drink = function(name, description, price, ingredients){
+	this.name = name;
+	this.description = description;
+	this.price = price;
+	this.ingredients = ingredients;
+};
+
+Drink.prototype.toString = drinkOrPlateToString;
+
+Drink.prototype.create = function() {
+  return $('<p>' + this + '</p>');
+}
+
+var Plate = function(name, description, price, ingredients){
+	this.name = name;
+	this.description = description;
+	this.price = price;
+	this.ingredients = ingredients;
+};
+
+Plate.prototype.toString = drinkOrPlateToString;
+
 Plate.prototype.isVegan = makeDietChecker('vegan');
 Plate.prototype.isGlutenFree = makeDietChecker('glutenFree');
 Plate.prototype.isCitrusFree = makeDietChecker('citrusFree');
+
+Plate.prototype.create = function() {
+  return $('<p>' + this + '</p>');
+};
+
+//
+// Order
+//
 
 var Order = function(plates){
 	this.plates = plates;
@@ -81,6 +101,20 @@ Order.prototype.toString = function(){
 	}).join(', ');
 };
 
+Order.prototype.create = function() {
+	var $order = $('<div></div>');
+
+	$('.plates').on('click', 'p', function() {
+		$order.append($(this).clone());
+	});
+
+	return $order;
+}
+
+//
+// Menu
+//
+
 var Menu = function(plates){
 	this.plates = plates;
 };
@@ -90,6 +124,20 @@ Menu.prototype.toString = function(){
 		return plate.toString();
 	}).join(' \n');
 };
+
+Menu.prototype.create = function() {
+  var $plates = $('<div class="plates"></div>');
+
+  for (var i = 0; i < this.plates.length; i++) {
+    $plates.append(this.plates[i].create());
+  }
+
+  return $plates;
+};
+
+//
+// Restaurant
+//
 
 var Restaurant = function(name, description, menu){
 	this.name = name;
@@ -102,13 +150,30 @@ Restaurant.prototype.toString = function(){
 		this.description + '\n\n' +
 		'Menu:\n' +
 		'--------\n' +
-		this.menu.toString(); 
+		this.menu.toString();
 };
 
+Restaurant.prototype.create = function() {
+  var $restaurant = $('<div></div>');
+
+  $restaurant.append($('<h1>' + this.name + '</h1>'))
+    .append($('<h2>' + this.description + '</h2>'))
+    .append(this.menu.create());
+
+  return $restaurant;
+}
+
+//
+// Customer
+//
 
 var Customer = function(dietaryPreference){
 	this.dietaryPreference = dietaryPreference;
 };
+
+//
+// Instances
+//
 
 var rice = new FoodItem('Rice', 200, true, true, true);
 var tortilla = new FoodItem('Tortilla', 150, true, false, true);
@@ -127,11 +192,11 @@ var firstOrder = new Order([burritoPlate, margarita, burritoPlate, guacamolePlat
 var mainMenu = new Menu([burritoPlate, guacamolePlate, margarita]);
 var restaurant = new Restaurant("Pica's Taqueria", 'A Mexican American Joint', mainMenu);
 
-// var granola = new FoodItem('granola', 200, true, true, true);
-// var steak = new FoodItem('steak', 300, false, true, true);
-// var pumpkinSeeds = new FoodItem('pumpkin seeds', 100, true, true, true);
+//
+// Page setup
+//
 
-// console.log(granola.toString());
-// console.log(steak.toString());
-// console.log(pumpkinSeeds.toString());
-// console.log(margarita.toString());
+$(document).ready(function() {
+  $('body').append(restaurant.create());
+	$('body').append((new Order([])).create());
+});
